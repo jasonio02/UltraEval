5f35fc21227831cad39dbe49ec4aa9b6270cc96d
jasonio02 <84747749+jasonio02@users.noreply.github.com>
2024-05-14 19:29:16 +0800

    Update transform_gen_v1.py

--- datasets/gsm8k/transform_gen_v1.py
+++ datasets/gsm8k/transform_gen_v1.py
@@ -1,3 +1,4 @@
+import re
 import random
 
 from UltraEval.tasks.postprocess import GSM8KPost
@@ -7,6 +8,10 @@ def transform(data, num_sample: int, r: random.Random, dataset_name: str):
     question = f"Question:\n{data['question']}?\n"
     answer_prompt = f"Answer:\n"
     text = question + answer_prompt
+    regex_text = re.findall(r'\b\d+\s*[\+\-\*/]\s*\d+(\s*[\+\-\*/]\s*\d+)*', data['question'])
+    if regex_text:
+        text += f"'\nMath Expressions:{','.join(regex_text)}\n"
+
     correct_answer = data["answer"]
     gsm8kp = GSM8KPost()
     _, processed_correct_answer = gsm8kp([], correct_answer)
